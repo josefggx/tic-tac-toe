b4f4fa1301c1bd151b1346115c5f9cdc0bb5d973
Jose Gutierrez <josefgg58@gmail.com>
2022-06-21 18:56:22 -0500

    Basic functionality to start game

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const displayController = (() => {
     const startMenu = document.querySelector('.start-menu');
     const vsBtn = document.querySelectorAll(".vs-btn");
     const gameContainer = document.querySelector('.game-container');
+    const backBtn = document.querySelector('#back-btn');
+    // @ts-ignore
+    const restartBtn = document.querySelector('#restart-btn');
 
     vsBtn.forEach((button) => {
         button.addEventListener('click', () => {
@@ -9,6 +12,56 @@ const displayController = (() => {
             startMenu.style.display = 'none';
             // @ts-ignore
             gameContainer.style.display = 'flex';
-        }); 
+            if(button.id == "vs-player-btn") {
+                // @ts-ignore
+                document.querySelector('#player-two-name').textContent = 'Player 2';
+                // @ts-ignore
+                document.querySelector('#player-two-icon').style.display = 'block';
+                // @ts-ignore
+                document.querySelector('#regular-ai-icon').style.display = 'none';
+                // @ts-ignore
+                document.querySelector('#invincible-ai-icon').style.display = 'none';
+            }
+            if(button.id == "vs-regular-AI-btn") {
+                // @ts-ignore
+                document.querySelector('#player-two-name').textContent = 'Regular AI';
+                // @ts-ignore
+                document.querySelector('#player-two-icon').style.display = 'none';
+                // @ts-ignore
+                document.querySelector('#regular-ai-icon').style.display = 'block';
+                // @ts-ignore
+                document.querySelector('#invincible-ai-icon').style.display = 'none';
+            }
+            if(button.id == "vs-invincible-AI-btn") {
+                // @ts-ignore
+                document.querySelector('#player-two-name').textContent = 'Invincible AI'
+                // @ts-ignore
+                document.querySelector('#player-two-icon').style.display = 'none';
+                // @ts-ignore
+                document.querySelector('#regular-ai-icon').style.display = 'none';
+                // @ts-ignore
+                document.querySelector('#invincible-ai-icon').style.display = 'block';
+            }
+        });
     });
+    // @ts-ignore
+    backBtn.addEventListener('click', () => {
+        // @ts-ignore
+        startMenu.style.display = 'flex';
+        // @ts-ignore
+        gameContainer.style.display = 'none';
+    })
+
 })();
+
+const gameBoard = (() => {
+    let gameboardArray = [];
+
+    const gameboardBoxes = document.querySelectorAll('.gameboard-box');
+    gameboardBoxes.forEach((box) => {
+        box.addEventListener('click', () => {
+            
+        });
+    });
+
+})()

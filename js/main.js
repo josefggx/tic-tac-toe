2a19c2b36c9c673ec60d77980a009a72c308c57e
Jose Gutierrez <josefgg58@gmail.com>
2022-06-21 21:41:18 -0500

    Added functions to make marks

--- js/main.js
+++ js/main.js
@@ -57,11 +57,66 @@ const displayController = (() => {
 const gameBoard = (() => {
     let gameboardArray = [];
 
+    const winCombinations = [
+        ["zero", "one", "two"],
+        ["three", "four", "five"],
+        ["six", "seven", "eight"],
+        ["zero", "three", "six"],
+        ["one", "four", "seven"],
+        ["two", "five", "eight"],
+        ["zero", "four", "eight"],
+        ["five", "four", "two"]
+    ];
+
+    const playerActions = {
+        changeTurn() {
+            if(this.turn === false) {
+                return this.turn = true;
+            } else if(this.turn === true) {
+                return this.turn = false;
+            }
+        },
+        markIdentifier(box) {
+            return this.marks.push(box.id);
+        }
+
+    };
+
+    const playerFactory = (name, marks, icon, turn) => {
+        let player = Object.create(playerActions);
+        player.name = name;
+        player.marks = marks;
+        player.icon = icon;
+        player.turn = turn;
+        return player;
+    }
+
+    let player1 = playerFactory("Player 1", [], "fa-solid fa-user-large", true);
+    let player2 = playerFactory("Player 2", [], "fa-solid fa-user-large", false);
+
     const gameboardBoxes = document.querySelectorAll('.gameboard-box');
-    gameboardBoxes.forEach((box) => {
-        box.addEventListener('click', () => {
-            
+    const playerGame = () => {
+        gameboardBoxes.forEach((box) => {
+            box.addEventListener('click', () => {
+                if(player1.turn === true && box.textContent === "") {
+                    box.textContent = "X";
+                    player1.markIdentifier(box);
+                    console.log(player1.marks);
+                    player1.changeTurn();
+                    player2.changeTurn();
+                }
+                else if(player2.turn === true && box.textContent === "") {
+                    box.textContent = "O";
+                    player2.markIdentifier(box);
+                    console.log(player2.marks);
+                    player2.changeTurn();
+                    player1.changeTurn();
+                }
+
+            });
         });
-    });
+    }
+    playerGame();
+
 
 })()
